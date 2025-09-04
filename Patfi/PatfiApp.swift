c5c94f439d8ea8b3af2eb6e45956c72ba834d22d
Mathieu Vandeginste <matvdg@me.com>
2025-09-04 19:00:38 +0200

    Initial Commit

---
+++ Patfi/PatfiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatfiApp.swift
+//  Patfi
+//
+//  Created by Mathieu Vandeginste on 04/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct PatfiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
