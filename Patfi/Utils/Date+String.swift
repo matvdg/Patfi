214edc02af4c4f06657a9446313fef14172f9168
Mathieu Vandeginste <matvdg@me.com>
2025-09-10 19:56:53 +0200

    graph segmented period + preview

---
+++ Patfi/Utils/Date+String.swift
@@ -0,0 +1,8 @@
+//
+//  Date+String.swift
+//  Patfi
+//
+//  Created by Mathieu Vandeginste on 10/09/2025.
+//
+
+import Foundation
---
+++ Patfi/Views/Dashboard/DashboardTotalView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardTotalView.swift
+//  Patfi
+//
+//  Created by Mathieu Vandeginste on 10/09/2025.
+//
+
+import SwiftUI
+
+struct DashboardTotalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardTotalView()
+}
