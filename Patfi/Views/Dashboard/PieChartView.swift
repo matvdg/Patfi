e356b9ecd1cf7c4a02301a6af37777757f6f6229
Mathieu Vandeginste <matvdg@me.com>
2025-09-11 12:44:37 +0200

    Many improvements

---
+++ Patfi/Views/Dashboard/PieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  PieChartView.swift
+//  Patfi
+//
+//  Created by Mathieu Vandeginste on 11/09/2025.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PieChartView()
+}
